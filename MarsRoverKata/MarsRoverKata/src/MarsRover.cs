cd14073ebe856e99687a6b2e77e74408d85aafbe
Yu-Lun <chou522128@gmail.com>
2024-05-04 21:31:41 +0800

    [Refactor] MarsRover.cs：get position in directions array

--- MarsRoverKata/MarsRoverKata/src/MarsRover.cs
+++ MarsRoverKata/MarsRoverKata/src/MarsRover.cs
@@ -8,12 +8,13 @@ namespace MarsRoverKata.src
 {
     public class MarsRover
     {
+        private string[] directions = new string[] { "N", "E", "S", "W" };
         public string Execute(string commands)
         {
             var charArr = commands.ToCharArray();
             var x = 0;
             var y = 0;
-            var position = "N";
+            var position = 0;
             
             foreach (char c in charArr)
             {
@@ -23,11 +24,11 @@ namespace MarsRoverKata.src
                 }
                 if (c == 'R')
                 {
-                    position = "E";
+                    position++;
                 }
             }
             
-            return $"{x}:{y}:{position}";
+            return $"{x}:{y}:{directions[position]}";
         }
     }
 }
