cf17b2421ea6f1b82fc11f06838e456d65005ec1
Yu-Lun <chou522128@gmail.com>
2024-05-17 23:10:15 +0800

    [Green] MarsRoverTest.cs - A04_EndOfTheGrid：add test case
    commands: MMMMMMLM
    position: 0:6:W

--- MarsRoverKata/MarsRoverKata/MarsRoverTest.cs
+++ MarsRoverKata/MarsRoverKata/MarsRoverTest.cs
@@ -48,6 +48,7 @@ namespace MarsRoverKata
         
         [TestCase("MMMMMMMMMMRM", "1:9:E")]
         [TestCase("RMMMMMMMMMMLM", "9:1:N")]
+        [TestCase("MMMMMMLM", "0:6:W")]
         public void A04_EndOfTheGrid(string commands, string position)
         {
             AssertResultShouldReturn(commands, position);
