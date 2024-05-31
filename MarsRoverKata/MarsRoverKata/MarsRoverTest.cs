ca94c0dd73f45ba093cc7d4ed01142a29af8b458
Yu-Lun <chou522128@gmail.com>
2024-05-31 22:34:11 +0800

    [Green] MarsRoverTest.cs - A03_TurnLeft：add test case
    commands: LLL
    position: 0:0:E

--- MarsRoverKata/MarsRoverKata/MarsRoverTest.cs
+++ MarsRoverKata/MarsRoverKata/MarsRoverTest.cs
@@ -38,6 +38,7 @@ namespace MarsRoverKata
         [Test]
         [TestCase("L", "0:0:W")]
         [TestCase("LL", "0:0:S")]
+        [TestCase("LLL", "0:0:E")]
         public void A03_TurnLeft(string commands, string position)
         {
             AssertResultShouldReturn(commands, position);
