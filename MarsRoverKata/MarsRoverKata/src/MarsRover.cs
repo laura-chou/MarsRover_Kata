a1fa14d9508d748546096436b4e3a2c021e1ffd2
Yu-Lun <chou522128@gmail.com>
2024-06-07 22:56:32 +0800

    [Refactor] MarsRover.cs：introduce variable and check M command

--- MarsRoverKata/MarsRoverKata/src/MarsRover.cs
+++ MarsRoverKata/MarsRoverKata/src/MarsRover.cs
@@ -8,9 +8,24 @@ namespace MarsRoverKata.src
 {
     public class MarsRover
     {
+        private string[] directions = new string[] { "N", "E", "S", "W" };
         public string Execute(string commands)
         {
-            return "0:1:N";
+            var x = 0;
+            var y = 0;
+            var position = 0;
+
+            foreach (var command in commands)
+            {
+                switch (command)
+                {
+                    case 'M':
+                        y++;
+                        break;
+                }
+            }
+
+            return $"{x}:{y}:{directions[position]}";
         }
     }
 }
