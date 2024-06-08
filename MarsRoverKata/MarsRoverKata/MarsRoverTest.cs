1807c4c0481ec161ac6a8c60037be1e47916c0ff
Yu-Lun <chou522128@gmail.com>
2024-06-08 21:43:36 +0800

    [Red] MarsRoverTest.cs - A02_RoverTurnRightAndForward：add test case
    commands: RRRR
    position: 0:0:N

--- MarsRoverKata/MarsRoverKata/MarsRoverTest.cs
+++ MarsRoverKata/MarsRoverKata/MarsRoverTest.cs
@@ -28,6 +28,7 @@ namespace MarsRoverKata
         [TestCase("R", "0:0:E")]
         [TestCase("RR", "0:0:S")]
         [TestCase("RRR", "0:0:W")]
+        [TestCase("RRRR", "0:0:N")]
         public void A02_RoverTurnRightAndForward(string commands, string position)
         {
             AssertResultShouldReturn(commands, position);
